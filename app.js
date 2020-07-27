9c0e0459fcf3e437d6232b4693c0907a7db12b4a
ahmedusama25 <ahmed.usama25@hotmail.com>
2020-07-27 23:19:53 +0500

    update code

--- app.js
+++
@@ -1,9 +0,0 @@
-function Add(){
-    var inpText = document.getElementById("inpText")
-    var list = document.getElementById("list")
-    var createNode = document.createElement("p")
-    var nodeText = document.createTextNode(inpText.value)
-    createNode.appendChild(nodeText)
-    list.appendChild(createNode)
-    inpText.value=""
-}
---
+++ public/app.js
@@ -0,0 +1,38 @@
+function addBtn(){
+    var getInput = document.getElementById("getInput")
+    var listElement= document.createElement("li")
+    var listText= document.createTextNode(getInput.value)
+    var todoList = document.getElementById("todoList")
+    listElement.appendChild(listText)
+    
+
+    var delbtn = document.createElement("button")
+    var delbtnText = document.createTextNode("delete")
+    delbtn.appendChild(delbtnText) 
+    listElement.appendChild(delbtn)
+    delbtn.setAttribute("onclick","delList(this)")
+
+    var updBtn = document.createElement("button")
+    var updBtnText = document.createTextNode("update")
+    updBtn.append(updBtnText)
+    listElement.appendChild(updBtn)
+    updBtn.setAttribute("onclick","updateList(this)")   
+    todoList.appendChild(listElement)
+
+
+    getInput.value = ""
+
+}
+
+
+function delList(e){
+    e.parentNode.remove()
+    // console.log(e.getAttribute)
+}
+
+function updateList(a){
+    var target = a.parentNode.firstChild
+    var inp = prompt("Enter the updated value",target.nodeValue)
+    target.nodeValue=inp
+
+}
